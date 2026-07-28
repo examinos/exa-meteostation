5da23e9ef22445299510ac5c1a6cf3b29078c24d
David Vodička <david@vodickovi.cz>
2026-07-28 20:02:58 +0200

    Refaktorin

--- src/application.c
+++ src/application.c
@@ -6,10 +6,6 @@ Measurement of wind speed and direction together with rainfall sensor. Data are 
 
 Original firmware https://github.com/hubmartin/bcf-sigfox-wind-station
 
-Connection of the sensor module:
-Channel A: anemometer (other contact connected to GND)
-Channel B: wind direction (other contact connected to GND)
-Channel C: rainfall switch (other contact connected to GND)
 
 Datasheet
 https://cdn.sparkfun.com/assets/8/4/c/d/6/Weather_Sensor_Assembly_Updated.pdf
@@ -32,18 +28,178 @@ https://www.ebay.co.uk/itm/Mounting-arm-for-wind-speed-wind-direction-rain-meter
 #include <application.h>
 #include <math.h>
 
-twr_led_t led;
-twr_tmp112_t temp;
 
+// ==============================================================================================================
+// Channel A: anemometer (other contact connected to GND)
+
+// If you use external pull-up >500kOhm to lower current through resistor from 100 uA to 1uA when the wind speed
+// vane is in a position where the magnetic reed is closed
+// Connect 1M resistor between VDD and A screw terminal
+//#define EXTERNAL_PULLUP_VCC_A
+
+#define WIND_DATA_STREAM_SAMPLES 40
+#define MAIN_TASK_PERIOD_SECONDS 15
+
+
+// Last speed for debug
+float windSpeed;
 float windSpeedAverage = 0;
 float windSpeedMaximum = 0;
-float windAngleAverage = 0;
-float batteryVoltage = 0;
 
-float rainMM = 0;              // mm of rainfall from last send data
-float rainTotalMM = 0;         // mm of rainfall total
+// Data stream for wind speed averaging
+TWR_DATA_STREAM_FLOAT_BUFFER(stream_buffer_wind_speed, WIND_DATA_STREAM_SAMPLES)
+twr_data_stream_t stream_wind_speed;
+
+
+void anemometer_init()
+{
+    twr_data_stream_init(&stream_wind_speed, 1, &stream_buffer_wind_speed);
+
+    // Pulse counter
+    twr_pulse_counter_init(TWR_MODULE_SENSOR_CHANNEL_A, TWR_PULSE_COUNTER_EDGE_FALL);
+    twr_pulse_counter_set_event_handler(TWR_MODULE_SENSOR_CHANNEL_A, NULL, NULL);
+
+    #ifdef EXTERNAL_PULLUP_VCC_A
+    // Disable internal pullup
+    twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_A, TWR_MODULE_SENSOR_PULL_NONE);
+    // Enable power to VDD to power the extenal resistor pullup
+    twr_module_sensor_set_vdd(true);
+    #endif
+
+}
+
+void anemometer_task()
+{
+    // Reading and reseting wind speed counter
+    float counter = (float)twr_pulse_counter_get(TWR_MODULE_SENSOR_CHANNEL_A);
+    twr_pulse_counter_reset(TWR_MODULE_SENSOR_CHANNEL_A);
+
+    // Get current wind speed, one pulse per second ~ 2.4kmph
+    windSpeed = (counter / ((float)MAIN_TASK_PERIOD_SECONDS)) * 0.66666f; // 2.4km/h ~ 0.66666m/s
+
+    // Save maximum wind speed value
+    if(windSpeed > windSpeedMaximum)
+    {
+        windSpeedMaximum = windSpeed;
+    }
+
+    // Add value to stream and get average
+    twr_data_stream_feed(&stream_wind_speed, &windSpeed);
+    twr_data_stream_get_average(&stream_wind_speed, &windSpeedAverage);
+
+    twr_log_debug("speed %f", windSpeed);
+
+}
+
+
+// ==============================================================================================================
+// Channel B: wind direction (other contact connected to GND)
+
+// Wind voltage table
+// This was measured with original resistor in wind direction sensor and internal MCU pullup
+// Some values are too close to each other. Better solution will be to use external 4k7 hardware pullup
+// and edit this table to the real ADC values.
+float windADCTable[16] = {
+    30845, // 0° North
+    9888, // 22.5°
+    11912, // 45°
+    1645, // 67.5°
+    1834, // 90° East
+    1307, // 112.5°
+    3774, // 135°
+    2488, // 157.5°
+    6283, // 180° South
+    5208, // 202.5°
+    19713, // 225°
+    18111, // 247.5°
+    50011, // 270° West
+    34788, // 292.5°
+    41535, // 315°
+    24288  // 337.5°
+};
+
+float windVoltageToAngle(float voltage)
+{
+    float smallestDifferenceValue = 1E8f; // Set big number
+    float smallestDifferenceAngle;
+
+    uint32_t i;
+
+    for(i = 0; i < 16; i++)
+    {
+        float currentDifference = fabs(voltage - windADCTable[i]);
+        if(smallestDifferenceValue > currentDifference)
+        {
+            smallestDifferenceValue = currentDifference;
+            smallestDifferenceAngle = 22.50f * i;
+        }
+    }
+
+    return smallestDifferenceAngle;
+}
+
+float windAdcVoltage = 0;
+uint16_t windAdc = 0;
+
+// Last angle  for debug
+float windAngle;
+float windAngleAverage = 0;
+
+void adc_event_handler(twr_adc_channel_t channel, twr_adc_event_t event, void *param)
+{
+    (void)param;
+    (void)channel;
+
+    if (event == TWR_ADC_EVENT_DONE)
+    {
+        if(channel == TWR_ADC_CHANNEL_A5)
+        {
+            //#define ADC_VALUE_TO_VOLTAGE2(__RESULT__)   (((__RESULT__) * (0.00004743)))
+
+            // Disable pullup
+            twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_PULL_NONE);
+
+            twr_adc_async_get_value(TWR_ADC_CHANNEL_A5, &windAdc);
+            //windAdcVoltage = ADC_VALUE_TO_VOLTAGE2(windAdc);
+            windAngle = windVoltageToAngle(windAdc);
+
+            angle_average_add(windAngle);
+            windAngleAverage = angle_average_get();
+
+            // Start battery measurement, default library collides sometimes with ADC measurement
+            twr_module_battery_measure();
+
+            twr_log_debug("adc: %d, angle: %f", windAdc, windAngle);
+        }
+    }
+}
+
+ 
+void windvane_init()
+{
+    twr_module_sensor_set_mode(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_MODE_INPUT);
+    // Pullup is enabled in the task just before measuring
+
+    // Initialize ADC channel - wind direction
+    twr_adc_init();
+    twr_adc_set_event_handler(TWR_ADC_CHANNEL_A5, adc_event_handler, NULL);
+    twr_adc_resolution_set(TWR_ADC_CHANNEL_A5, TWR_ADC_RESOLUTION_12_BIT);
+
+}
+
+void windvane_task()
+{
+    // Enable pullup during wind direction measurement
+    twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_PULL_UP_INTERNAL);
+    twr_adc_async_measure(TWR_ADC_CHANNEL_A5);
+}
+
+
+// ==============================================================================================================
+// Channel C: rainfall switch (other contact connected to GND)
 
 /*
+
 The rainfall should be calibrated
 Measure the area of the rainfall sensor in centimeters
 inspiration by https://www.instructables.com/id/Arduino-Rain-Gauge-Calibration/
@@ -62,27 +218,56 @@ My sensor trips with 1.6mm
 
 1.6 cm^3 / 55 cm^3 = 0.029090909.. cm = 0.29090 mm
 */
-
 #define RAINFALL_PULSE_MM 0.29090f    // millimiters of rainfall in a single pulse from the sensor
+
+twr_switch_t rain_gauge;
+
+float rainMM = 0;              // mm of rainfall from last send data
+float rainTotalMM = 0;         // mm of rainfall total
+
+// Handler for rain gauge interrupt signals
+void rain_counter_handler(twr_switch_t *self, twr_switch_event_t event, void *event_param){
+    if (event == TWR_SWITCH_EVENT_OPENED)
+    {
+        rainTotalMM += RAINFALL_PULSE_MM;
+        rainMM += RAINFALL_PULSE_MM;
+    }
+}
+
+
+void rain_init()
+{
+    // Rain counter
+    twr_switch_init(&rain_gauge, TWR_GPIO_P7, TWR_SWITCH_TYPE_NC, TWR_SWITCH_PULL_UP_DYNAMIC);
+    twr_switch_set_event_handler(&rain_gauge, rain_counter_handler, NULL);
+
+}
+
+void rain_task()
+{
+    
+}
+
+// ==============================================================================================================
+
+
+
+
+
+
+twr_led_t led;
+twr_tmp112_t temp;
+
+float batteryVoltage = 0;
+
+
 
 float internalTemperature = 0;
 float internalTemperatureAverage = 0;
-twr_switch_t rain_gauge;
 
-#define MAIN_TASK_PERIOD_SECONDS 15
 #define TRANSMIT_PERIOD_MINUTES 1
-#define WIND_DATA_STREAM_SAMPLES 40
 
-// If you use external pull-up >500kOhm to lower current through resistor from 100 uA to 1uA when the wind speed
-// vane is in a position where the magnetic reed is closed
-// Connect 1M resistor between VDD and A screw terminal
-//#define EXTERNAL_PULLUP_VCC_A
 
-// Data stream for wind speed averaging
-TWR_DATA_STREAM_FLOAT_BUFFER(stream_buffer_wind_speed, WIND_DATA_STREAM_SAMPLES)
-TWR_DATA_STREAM_FLOAT_BUFFER(stream_buffer_internal_temperature, WIND_DATA_STREAM_SAMPLES)
-twr_data_stream_t stream_wind_speed;
-twr_data_stream_t stream_internal_temperature;
 
 //#define CLIMATE_ENABLE
 
@@ -117,34 +302,8 @@ struct {
     event_param_t pressure;
 } params;
 
-// Wind voltage table
-// This was measured with original resistor in wind direction sensor and internal MCU pullup
-// Some values are too close to each other. Better solution will be to use external 4k7 hardware pullup
-// and edit this table to the real ADC values.
-float windADCTable[16] = {
-    30845, // 0° North
-    9888, // 22.5°
-    11912, // 45°
-    1645, // 67.5°
-    1834, // 90° East
-    1307, // 112.5°
-    3774, // 135°
-    2488, // 157.5°
-    6283, // 180° South
-    5208, // 202.5°
-    19713, // 225°
-    18111, // 247.5°
-    50011, // 270° West
-    34788, // 292.5°
-    41535, // 315°
-    24288  // 337.5°
-};
 
-// Last angle and speed for debug
-float windAngle;
-float windSpeed;
-
-void adc_event_handler(twr_adc_channel_t channel, twr_adc_event_t event, void *param);
+//void adc_event_handler(twr_adc_channel_t channel, twr_adc_event_t event, void *param);
 
 // Wind data transmitting function
 void publish_wind_task(void *param)
@@ -189,7 +348,6 @@ void publish_rain_task(void *param)
 
     twr_log_debug("rain mm: %f total mm: %f", rainMM, rainTotalMM);
 
-    //twr_log_debug("adc: %d, angle: %f", windAdc, windAngle);
 
     // Reset rain
     rainMM = 0;
@@ -198,28 +356,7 @@ void publish_rain_task(void *param)
     twr_scheduler_plan_current_relative(1000 * 60 * TRANSMIT_PERIOD_MINUTES);
 }
 
-float windVoltageToAngle(float voltage)
-{
-    float smallestDifferenceValue = 1E8f; // Set big number
-    float smallestDifferenceAngle;
 
-    uint32_t i;
-
-    for(i = 0; i < 16; i++)
-    {
-        float currentDifference = fabs(voltage - windADCTable[i]);
-        if(smallestDifferenceValue > currentDifference)
-        {
-            smallestDifferenceValue = currentDifference;
-            smallestDifferenceAngle = 22.50f * i;
-        }
-    }
-
-    return smallestDifferenceAngle;
-}
-
-float windAdcVoltage = 0;
-uint16_t windAdc = 0;
 
 void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *event_param)
 {
@@ -232,43 +369,7 @@ void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *ev
     }
 }
 
-void adc_event_handler(twr_adc_channel_t channel, twr_adc_event_t event, void *param)
-{
-    (void)param;
-    (void)channel;
 
-    if (event == TWR_ADC_EVENT_DONE)
-    {
-        if(channel == TWR_ADC_CHANNEL_A5)
-        {
-            //#define ADC_VALUE_TO_VOLTAGE2(__RESULT__)   (((__RESULT__) * (0.00004743)))
-
-            // Disable pullup
-            twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_PULL_NONE);
-
-            twr_adc_async_get_value(TWR_ADC_CHANNEL_A5, &windAdc);
-            //windAdcVoltage = ADC_VALUE_TO_VOLTAGE2(windAdc);
-            windAngle = windVoltageToAngle(windAdc);
-
-            angle_average_add(windAngle);
-            windAngleAverage = angle_average_get();
-
-            // Start battery measurement, default library collides sometimes with ADC measurement
-            twr_module_battery_measure();
-
-            twr_log_debug("adc: %d, angle: %f", windAdc, windAngle);
-        }
-    }
-}
-
-// Handler for rain gauge interrupt signals
-void rain_counter_handler(twr_switch_t *self, twr_switch_event_t event, void *event_param){
-    if (event == TWR_SWITCH_EVENT_OPENED)
-    {
-        rainTotalMM += RAINFALL_PULSE_MM;
-        rainMM += RAINFALL_PULSE_MM;
-    }
-}
 
 
 void climate_module_event_handler(twr_module_climate_event_t event, void *event_param)
@@ -339,9 +440,12 @@ void climate_module_event_handler(twr_module_climate_event_t event, void *event_
     }
 }
 
+    TWR_DATA_STREAM_FLOAT_BUFFER(stream_buffer_internal_temperature, WIND_DATA_STREAM_SAMPLES)
+    twr_data_stream_t stream_internal_temperature;
+
+
 void application_init(void)
 {
-    twr_data_stream_init(&stream_wind_speed, 1, &stream_buffer_wind_speed);
 
     twr_log_init(TWR_LOG_LEVEL_DEBUG, TWR_LOG_TIMESTAMP_ABS);
 
@@ -356,28 +460,15 @@ void application_init(void)
     // set measurement handler (call "tmp112_event_handler()" after measurement)
     twr_tmp112_set_event_handler(&temp, tmp112_event_handler, NULL);
 
-    // Pulse counter
-    twr_pulse_counter_init(TWR_MODULE_SENSOR_CHANNEL_A, TWR_PULSE_COUNTER_EDGE_FALL);
-    twr_pulse_counter_set_event_handler(TWR_MODULE_SENSOR_CHANNEL_A, NULL, NULL);
 
-    #ifdef EXTERNAL_PULLUP_VCC_A
-    // Disable internal pullup
-    twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_A, TWR_MODULE_SENSOR_PULL_NONE);
-    // Enable power to VDD to power the extenal resistor pullup
-    twr_module_sensor_set_vdd(true);
-    #endif
+    anemometer_init();
+    windvane_init();
+    rain_init();
 
-    // Rain counter
-    twr_switch_init(&rain_gauge, TWR_GPIO_P7, TWR_SWITCH_TYPE_NC, TWR_SWITCH_PULL_UP_DYNAMIC);
-    twr_switch_set_event_handler(&rain_gauge, rain_counter_handler, NULL);
 
-    twr_module_sensor_set_mode(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_MODE_INPUT);
-    // Pullup is enabled in the task just before measuring
 
-    // Initialize ADC channel - wind direction
-    twr_adc_init();
-    twr_adc_set_event_handler(TWR_ADC_CHANNEL_A5, adc_event_handler, NULL);
-    twr_adc_resolution_set(TWR_ADC_CHANNEL_A5, TWR_ADC_RESOLUTION_12_BIT);
+
+
 
     // Battery module voltage
     twr_module_battery_init();
@@ -410,33 +501,16 @@ void application_init(void)
 
 void application_task()
 {
-    // Enable pullup during wind direction measurement
-    twr_module_sensor_set_pull(TWR_MODULE_SENSOR_CHANNEL_B, TWR_MODULE_SENSOR_PULL_UP_INTERNAL);
-    twr_adc_async_measure(TWR_ADC_CHANNEL_A5);
+    anemometer_task();
+    windvane_task();
+    rain_task();
 
-    // Reading and reseting wind speed counter
-    float counter = (float)twr_pulse_counter_get(TWR_MODULE_SENSOR_CHANNEL_A);
-    twr_pulse_counter_reset(TWR_MODULE_SENSOR_CHANNEL_A);
-
-    // Get current wind speed, one pulse per second ~ 2.4kmph
-    windSpeed = (counter / ((float)MAIN_TASK_PERIOD_SECONDS)) * 0.66666f; // 2.4km/h ~ 0.66666m/s
-
-    // Save maximum wind speed value
-    if(windSpeed > windSpeedMaximum)
-    {
-        windSpeedMaximum = windSpeed;
-    }
-
-    // Add value to stream and get average
-    twr_data_stream_feed(&stream_wind_speed, &windSpeed);
-    twr_data_stream_get_average(&stream_wind_speed, &windSpeedAverage);
 
     // Read temperature and save it to the buffer stream
     twr_tmp112_measure(&temp);
     twr_data_stream_feed(&stream_internal_temperature, &internalTemperature);
     twr_data_stream_get_average(&stream_internal_temperature, &internalTemperatureAverage);
 
-    twr_log_debug("speed %f", windSpeed);
 
     twr_scheduler_plan_current_relative(MAIN_TASK_PERIOD_SECONDS * 1000);
 }
